d3dd978285d05eeff6c6f641234709102e5ade66
Matteo Polito <mapo80@gmail.com>
2025-08-26 09:31:07 +0200

    Handle disposed Hangfire activator in job state filter

--- src/DocflowAi.Net.Api/JobQueue/Filters/JobStateFilter.cs
+++ src/DocflowAi.Net.Api/JobQueue/Filters/JobStateFilter.cs
@@ -14,11 +14,20 @@ public class JobStateFilter : IApplyStateFilter
             context.BackgroundJob.Job.Args.Count > 0 &&
             context.BackgroundJob.Job.Args[0] is Guid jobId)
         {
-            using var scope = JobActivator.Current.BeginScope();
-            var repo = (IJobRepository)scope.Resolve(typeof(IJobRepository));
-            var uow = (IUnitOfWork)scope.Resolve(typeof(IUnitOfWork));
-            repo.UpdateStatus(jobId, "Queued");
-            uow.SaveChanges();
+            try
+            {
+                #pragma warning disable CS0618 // JobActivator.BeginScope is obsolete in Hangfire 1.x
+                using var scope = JobActivator.Current.BeginScope();
+                #pragma warning restore CS0618
+                var repo = (IJobRepository)scope.Resolve(typeof(IJobRepository));
+                var uow = (IUnitOfWork)scope.Resolve(typeof(IUnitOfWork));
+                repo.UpdateStatus(jobId, "Queued");
+                uow.SaveChanges();
+            }
+            catch (ObjectDisposedException)
+            {
+                // server shutting down, safe to ignore
+            }
         }
     }
 
